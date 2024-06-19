2c5647a04079ef993d3ca3c228ce0300c4884298
Greg <11791585+elphick@users.noreply.github.com>
2024-06-19 15:53:22 +0800

    skip examples test in ci-cd

--- tests/test_100_examples.py
+++ tests/test_100_examples.py
@@ -16,7 +16,12 @@ modules_to_test: list[str] = [
     for p in example_files
 ]
 
+# Check if we are in a CI/CD environment
+on_ci_cd = os.environ.get('ON_CI_CD', 'false').lower() == 'true'
+
+
 @pytest.mark.parametrize("module_name", modules_to_test)
+@pytest.mark.skipif(on_ci_cd, reason="Skip on CI/CD")
 def test_examples(module_name):
     os.environ["PYVISTA_OFF_SCREEN"] = "True"
     __import__(module_name)
