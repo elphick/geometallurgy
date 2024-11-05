13e7c3e8d4f635700012811f460c3d0f3b1c7697
Greg <11791585+elphick@users.noreply.github.com>
2024-11-05 21:26:47 +0800

    fs.to_dataframe column order fix #3

--- elphick/geomet/flowsheet/flowsheet.py
+++ elphick/geomet/flowsheet/flowsheet.py
@@ -709,8 +709,10 @@ class Flowsheet:
         results: pd.DataFrame = pd.concat(chunks, axis='index').set_index('name', append=True)
         if not tidy:  # wide format
             results = results.unstack(level='name', sort=False)
-            results.columns = [f'{name}_{attr}' for name in results.columns.levels[1] for attr in
-                               results.columns.levels[0]]
+            column_order: list[str] = [f'{name}_{attr}' for name in results.columns.levels[1] for attr in
+                                       results.columns.levels[0]]
+            results.columns = [f'{col[1]}_{col[0]}' for col in results.columns]
+            results = results[column_order]
 
         return results
 
