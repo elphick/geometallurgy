9abff5fdd0aecfd5f3592c53edd41a70bd197493
Greg <11791585+elphick@users.noreply.github.com>
2025-03-02 18:59:23 +0800

    removed omf file from assets and pointed example to the same asset in another repo using pooch.

--- examples/04_block_model/01_consuming_omf.py
+++ examples/04_block_model/01_consuming_omf.py
@@ -4,6 +4,7 @@ Consuming OMF
 
 This example demonstrates how to consume an Open Mining Format file
 """
+import tempfile
 from pathlib import Path
 
 import pandas as pd
@@ -12,27 +13,35 @@ from omfpandas import OMFPandasReader
 
 from elphick.geomet.block_model import BlockModel
 
-
 # %%
 # Load
 # ----
 
 # Cache an omf2 file locally
-# filepath: Path = Path(pooch.retrieve(
-#     url="https://github.com/elphick/omfpandas/blob/38-add-pyvista-plot-functionality/assets/v2/copper_deposit.omf",
-#     known_hash=None,
-#     path=Path(tempfile.gettempdir()) / "geometallurgy", ))
-
-filepath: Path = Path('../../assets/copper_deposit.omf')
+filepath: Path = Path(pooch.retrieve(
+    url="https://raw.githubusercontent.com/elphick/omfpandas/main/assets/copper_deposit.omf",
+    known_hash=None,
+    path=Path(tempfile.gettempdir()) / "geometallurgy"))
 
 omfpr: OMFPandasReader = OMFPandasReader(filepath=filepath)
 blocks: pd.DataFrame = omfpr.read_blockmodel(blockmodel_name='Block Model')
+
+# %%
+# Create mass
+# -----------
+# The mass of each block is calculated from the volume and a density of 2.265 g/cm3.
 
 blocks.reset_index(level=['dx', 'dy', 'dz'], inplace=True)
 blocks['volume'] = blocks.dx * blocks.dy * blocks.dz
 blocks.reset_index().set_index(keys=['x', 'y', 'z', 'dx', 'dy', 'dz'], inplace=True)
 blocks.rename(columns={'CU_pct': 'Cu'}, inplace=True)
 blocks['mass'] = blocks['volume'] * 2.265
+
+# %%
+# Create a BlockModel
+# -------------------
+# The BlockModel is created from the DataFrame and visualised.
+# This model can be used in a Flowsheet model to make metallurgical predictions and preserve the spatial context.
 
 bm: BlockModel = BlockModel(name=filepath.stem, data=blocks, mass_dry_var='mass', moisture_in_scope=False)
 p = bm.plot(scalar='Cu')
